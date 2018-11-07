8044de4d793769bb07a3680ff2a6cd677b964501
DoctorWhiskers <stuckonamine@gmail.com>
2018-11-07 13:03:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ bot.on("message", async message => {
             coins: 0
         };
     }
-    if(`${bot.user.username}`){
+    if(bot.user.username){
         let prefixEmbed = new Discord.RichEmbed()
         .setAuthor(message.author.username, message.author.displayAvatarURL)
         .setColor(aqua)
