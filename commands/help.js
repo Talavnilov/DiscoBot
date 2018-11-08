95fb5fbdcbe67fa47889f44e34b9954c1f81fa3d
DoctorWhiskers <stuckonamine@gmail.com>
2018-11-08 08:44:13 -0500

    Update commands/help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Basic Commands", "`coins` `botinfo` `help` `level` `pay` `ping` `report` `serverinfo` `warnlevel`")
     .addField("Staff Commands", "`clear` `kick` `warn` `tempmute`")
     .addField("Developer Commands", "`addrole` `removerole` `say`")
-    .addField("General Info About The Bot", `This bot was created to server multiple servers. This bot was made by ${Client.fetchUser(329452869897945088)}`);
+    .addField("General Info About The Bot", `This bot was created to server multiple servers. This bot was made by ${bot.users.get('329452869897945088').tag}`);
     return message.author.send(botembed)
 }
 module.exports.help = {
