ff9936051889e4e7b32ae1456625a8eee8874398
DoctorWhiskers <stuckonamine@gmail.com>
2018-11-08 09:05:02 -0500

    Update commands/help.js

--- commands/help.js
+++ commands/help.js
@@ -4,8 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let helpSent = new Discord.RichEmbed()
     .setColor("#00ddff")
     .setDescription(`✔ Sent you my help menu, please check your PMs!`);
-    message.channels.send(helpSent);
-    message.delete().catch(O_o=>{});
+    message.channel.send(helpSent);
 
     let botembed = new Discord.RichEmbed()
     .setTitle("Bot Help")
@@ -14,7 +13,8 @@ module.exports.run = async (bot, message, args) => {
     .addField("Staff Commands", "`clear` `kick` `warn` `tempmute`")
     .addField("Developer Commands", "`addrole` `removerole` `say`")
     .addField("General Info About The Bot", `This bot was created to server multiple servers. This bot was made by ${bot.users.get('329452869897945088').tag}`);
-    return message.author.send(botembed)
+    message.delete().catch(O_o=>{});
+    return message.author.send(botembed);
 }
 module.exports.help = {
     name: "help"
